958cbe444d6f02c648b0a453f1b869d352a060ce
Danylo <114859769+Almixs@users.noreply.github.com>
2025-05-06 20:49:09 +0300

    del info

--- pythonProject - Copy/telecom_package/exceptions.py
+++
@@ -1,7 +0,0 @@
-class DeviceError(Exception):
-    pass
-
-class DeviceConnectionError(DeviceError):
-    def __init__(self, message="Помилка з'єднання з пристроєм"):
-        self.message = message
-        super().__init__(self.message)
